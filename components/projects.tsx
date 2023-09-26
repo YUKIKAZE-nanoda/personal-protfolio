d59f2849524c9e9357ff932db704beae014d09fb
shigure <shigure_desu@icloud.com>
2023-09-26 12:48:51 +0800

    first commit

---
+++ components/projects.tsx
@@ -0,0 +1,34 @@
+"use client"
+import React, { useEffect } from 'react'
+import SectionHeading from './section-heading';
+import { projectsData } from '@/lib/data';
+import Project from './project';
+import { useSectionInView } from '@/lib/hooks';
+
+export default function Projects() {
+
+    const { ref } = useSectionInView("Projects", 0.5);
+    // const { ref, inView } = useInView({
+    //     threshold: 0.5,
+    // });
+    // const { setActiveSection } = useActiveSectionContext();
+    // useEffect(() => {
+    //     if (inView) {
+    //         setActiveSection("Projects");
+    //     }
+    // }, [inView, setActiveSection])
+    return (
+        <section ref={ref} id='projects' className='scroll-mt-28 mb-28'>
+            <SectionHeading>My Projects</SectionHeading>
+            <div>
+                 {
+                    projectsData.map((project, index) => (
+                        <React.Fragment key={index}>
+                            <Project {...project} />
+                        </React.Fragment>
+                    ))
+                }
+            </div>
+        </section>
+    );
+}
